aee2491e90e624475b92b6d54a4c709fcb5f2427
Lamberto Martinez <lambpato@gmail.com>
2023-02-28 19:58:35 +0000

    added the variable data to the data.js file and created the function getLocation on main.js

--- js/data.js
+++ js/data.js
@@ -1,1 +1,21 @@
 /* exported data */
+
+var data = {
+  view: 'entry-form',
+  locations: [],
+  editing: null,
+  nextEntryId: 1
+};
+
+var previousDataJSON = localStorage.getItem('local-storage');
+
+if (previousDataJSON != null) {
+  data = JSON.parse(previousDataJSON);
+}
+
+function storeData(e) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('local-storage', dataJSON);
+}
+
+window.addEventListener('beforeunload', storeData);
---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+// function getLocation(locations) {
+//   var xhr = new XMLHttpRequest();
+//   xhr.open('GET', 'http://api.weatherapi.com/v1/forecast.json?key=bb2468a183ea4225855173630232702&q=' + locations + '&days=8&aqi=yes&alerts=yes');
+//   xhr.responseType = 'json';
+//   xhr.addEventListener('load', function () {
+//     console.log(xhr.status);
+//     console.log(xhr.response);
+//   });
+//   xhr.send();
+// }
