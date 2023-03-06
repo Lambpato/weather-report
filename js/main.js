a3447b7aaec30d8216fa8a72b55df3f27d3fe096
Lamberto Martinez <lambpato@gmail.com>
2023-03-06 22:04:43 +0000

    added cors

--- js/main.js
+++ js/main.js
@@ -19,8 +19,9 @@ async function submitForm(e) {
 
 async function grabForecast(location) {
   return new Promise(function (resolve, reject) {
+    var targetUrl = encodeURIComponent('http://api.weatherapi.com/v1/forecast.json?key=bb2468a183ea4225855173630232702&q=' + location + '&days=8&aqi=yes&alerts=yes');
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://lfz-cors.herokuapp.com/?url=' + encodeURIComponent('http://api.weatherapi.com/v1/forecast.json?key=bb2468a183ea4225855173630232702&q=' + location + '&days=8&aqi=yes&alerts=yes'));
+    xhr.open('GET', 'https://lfz-cors.herokuapp.com/?url=' + targetUrl);
     xhr.setRequestHeader('token', 'abc123');
     xhr.responseType = 'json';
     xhr.onload = function () {
